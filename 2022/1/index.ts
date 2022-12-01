05af5b1454a62cfb2551d40788d92311e63a220d
Mange99 <magnus.bryntheson@hotmail.com>
2022-12-01 06:19:04 +0100

    First day 2022

--- 2022/1/index.ts
+++ 2022/1/index.ts
@@ -1,9 +1,26 @@
 import { FileReader } from "../../lib/FileReader";
 
-const data = FileReader.readFile("\n");
+const data = FileReader.readFileAsInt("\n");
 
-const part1 = () => {
-  console.log("lets go");
+let total: number[] = [];
+
+const part1 = (): number => {
+  let tot = 0;
+
+  data.map((value) => {
+    if (value > 0) {
+      tot += value;
+    } else {
+      total.push(tot);
+      tot = 0;
+    }
+  });
+  total = total.sort((a, b) => b - a);
+
+  return total[0];
 };
 
-const part2 = () => {};
+const part2 = (): number => total[0] + total[1] + total[2];
+
+console.log(part1());
+console.log(part2());
