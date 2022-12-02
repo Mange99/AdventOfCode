7688ed0ab3770c0d52b16ac3ae2d36be67623ac6
Mange99 <magnus.bryntheson@hotmail.com>
2022-12-02 07:18:53 +0100

    Day 2 cleanUp

--- 2022/2/index.ts
+++ 2022/2/index.ts
@@ -20,19 +20,15 @@ const part2 = (): number => {
   return data.reduce((prev, curr) => {
     let [x, y] = curr.split(" ");
     if (y == "X") {
-      if (x == "A") {
-        y = hands[1][2];
-      } else {
-        y = hands[1][hands[0].indexOf(x) - 1];
-      }
-      return prev + hands[1].indexOf(y) + 1;
+      y = hands[1][hands[0].indexOf(x) - 1 < 0 ? 2 : hands[0].indexOf(x) - 1];
     } else if (y == "Y") {
       y = hands[1][hands[0].indexOf(x)];
-      return prev + 3 + hands[1].indexOf(y) + 1;
+      prev += 3;
     } else {
       y = hands[1][(hands[0].indexOf(x) + 1) % 3];
-      return prev + 6 + hands[1].indexOf(y) + 1;
+      prev += 6;
     }
+    return prev + hands[1].indexOf(y) + 1;
   }, 0);
 };
 
