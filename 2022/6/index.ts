90d492b32aef6a3840abd8449899c4c6f3724d1e
Mange99 <magnus.bryntheson@hotmail.com>
2022-12-06 06:58:55 +0100

    cleanUp

--- 2022/6/index.ts
+++ 2022/6/index.ts
@@ -8,10 +8,7 @@ const part1 = (data: string): number => {
   const signals = data.split("");
 
   for (let i = 0; i < signals.length; i++) {
-    if (containsDuplicates(signals.slice(i, i + 4))) {
-    } else {
-      return i + 4;
-    }
+    if (!containsDuplicates(signals.slice(i, i + 4))) return i + 4;
   }
   return -1;
 };
@@ -20,16 +17,10 @@ const part2 = (data: string): number => {
   const signals = data.split("");
 
   for (let j = 0; j < signals.length; j++) {
-    if (containsDuplicates(signals.slice(j, j + 14))) {
-    } else {
-      return j + 14;
-    }
+    if (!containsDuplicates(signals.slice(j, j + 14))) return j + 14;
   }
   return -1;
 };
-
-// console.log(part1(data));
-// console.log(part2(data));
 
 Timed(1, () => part1(data));
 Timed(2, () => part2(data));
