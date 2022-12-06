fb61091618fde28462ff2c7b5beb1f954e669bd0
Mange99 <magnus.bryntheson@hotmail.com>
2022-12-06 15:03:44 +0100

    get alphabet nr

--- 2022/3/index.ts
+++ 2022/3/index.ts
@@ -1,8 +1,8 @@
 import { FileReader } from "../../lib/FileReader";
+import { alphabetNr } from "../../lib/StringHelper";
 import { Timed } from "../../lib/Timed";
 
 const data = FileReader.readFile();
-const alp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const part1 = (data: string): number =>
   data.split("\n").reduce((prev, curr) => {
@@ -11,7 +11,7 @@ const part1 = (data: string): number =>
     for (let i = 0; i < rucksack1.length; i++) {
       for (let j = 0; j < rucksack2.length; j++) {
         if (rucksack1[i] == rucksack2[j])
-          return prev + alp.indexOf(rucksack1[i]) + 1;
+          return prev + alphabetNr(rucksack1[i]);
       }
     }
     return prev;
@@ -29,7 +29,7 @@ const part2New = (data: string): number => {
     set1.forEach((value1) => {
       if (set2.has(value1)) {
         if (set3.has(value1)) {
-          count += alp.indexOf(value1) + 1;
+          count += alphabetNr(value1);
         }
       }
     });
@@ -37,36 +37,5 @@ const part2New = (data: string): number => {
   return count;
 };
 
-/*
-
-Worst time
-
-
-const overlap = (arr1: string[], arr2: string[]) =>
-  arr1.filter((value) => arr2.indexOf(value) !== -1);
-
-const part2 = (data: string): number => {
-  let count = 0;
-  const data2 = data.split("\n");
-
-  for (let x = 0; x < data2.length; x += 3) {
-    let inBoth = "";
-    const s1s2 = overlap(data2[x].split(""), data2[x + 1].split(""));
-    const s3 = data2[x + 2].split("");
-
-    for (let i = 0; i < s1s2.length; i++) {
-      for (let j = 0; j < s3.length; j++) {
-        if (s1s2[i] == s3[j]) {
-          inBoth = s1s2[i];
-          count += alp.indexOf(inBoth) + 1;
-          break;
-        }
-      }
-      if (inBoth) break;
-    }
-  }
-  return count;
-};
-*/
 Timed(1, () => part1(data));
 Timed(2, () => part2New(data));
