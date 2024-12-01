a777979a6580eaee52fe763c79fc2f44f2b41195
Mange99 <magnus.bryntheson@hotmail.com>
2024-12-01 14:00:27 +0100

    Day uno

--- 2024/day-1/index.ts
+++ 2024/day-1/index.ts
@@ -1,29 +1,51 @@
-import { log } from "console";
 import { FileReader } from "../../lib/FileReader";
 import { Timed } from "../../lib/Timed";
 
 const data = FileReader.readFile();
 
 const part1 = (data: string) => {
-  data
-    .split("\n\n")
-    .map((value) =>
-      value
-        .split("\n")
-        .map(Number)
-        .reduce((prev, curr) => prev + curr, 0)
-    )
-    .sort((a, b) => b - a)[1];
+  const [firstSet, secondSet] = getSortedLists(data);
+
+  return firstSet.reduce((prev, curr, i) => {
+    return prev + Math.abs(curr - secondSet[i]);
+  }, 0);
 };
 
 const part2 = (data: string) => {
-  // data.split("\n\n").map((value) => {
-  //   console.log(value);
-  // });
+  const [firstSet, secondSet] = getSortedLists(data);
+
+  let sum = 0;
+
+  for (let i = 0; i < firstSet.length; i++) {
+    let appears = 0;
+
+    for (let j = 0; j < secondSet.length; j++) {
+      if (secondSet[j] == firstSet[i]) {
+        appears++;
+      }
+    }
+
+    sum += firstSet[i] * appears;
+  }
+
+  return sum;
 };
 
-console.log(part1(data));
-// console.log(part2(data));
+const getSortedLists = (data: string) => {
+  const firstSet: number[] = [];
+  const secondSet: number[] = [];
 
-// Timed(1, () => part1(data));
-// Timed(2, () => part2(data));
+  data.split("\n").map((row) => {
+    const [left, right] = row.split("  ").map(Number);
+    firstSet.push(left);
+    secondSet.push(right);
+  });
+
+  firstSet.sort();
+  secondSet.sort();
+
+  return [firstSet, secondSet];
+};
+
+Timed(1, () => part1(data));
+Timed(2, () => part2(data));
